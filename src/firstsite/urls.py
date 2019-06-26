b56301bb6c7a27b093f8796b0326489d0f73403f
cand0701 <cand0701@naver.com>
2019-06-26 17:17:26 +0900

    css적용

--- src/firstsite/urls.py
+++ src/firstsite/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^apps/', include('apps.urls')),
+    url('', include('apps.urls')),
     url(r'^admin/', admin.site.urls),
 ]
